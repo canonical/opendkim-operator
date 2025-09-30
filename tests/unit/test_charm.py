144eb9c0264069e13a6699ddd64d2677b1ddf992
Javier de la Puente <javier.delapuente@canonical.com>
2025-09-30 16:17:00 +0200

    Minimal OpenDKIM charm that only installs opendkim

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,31 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+# pylint: disable=duplicate-code,missing-function-docstring
+"""Unit tests."""
+
+from unittest.mock import MagicMock
+
+import ops
+import ops.testing
+
+from charm import OpenDKIMCharm
+
+
+def test_install(monkeypatch):
+    """
+    arrange: Mock apt.add_package and prepare a trivial context and state.
+    act: Run install hook.
+    assert: Add package was called and the unit is active.
+    """
+    add_package_mock = MagicMock()
+    monkeypatch.setattr("charm.apt.add_package", add_package_mock)
+
+    context = ops.testing.Context(
+        charm_type=OpenDKIMCharm,
+    )
+    base_state: dict[str, str] = {}
+    state = ops.testing.State(**base_state)
+    out = context.run(context.on.install(), state)
+    add_package_mock.assert_called()
+    assert out.unit_status == ops.testing.ActiveStatus("")
