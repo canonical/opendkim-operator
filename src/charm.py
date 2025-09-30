c9578ac1bab2dadac247eca8142785464bd40c6c
Javier de la Puente <javier.delapuente@canonical.com>
2025-09-30 17:47:43 +0200

    feat: intial config file

--- src/charm.py
+++ src/charm.py
@@ -8,15 +8,20 @@
 """OpenDKIM charm."""
 
 import logging
+import os
 import typing
+from pathlib import Path
 
 import ops
 from charms.operator_libs_linux.v0 import apt
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 logger = logging.getLogger(__name__)
 
 OPENDKIM_PACKAGE_NAME = "opendkim"
 OPENDKIM_MILTER_PORT = 8892
+OPENDKIM_CONFIG_TEMPLATE = Path("opendkim.conf.j2")
+OPENDKIM_CONFIG_PATH = Path("/etc/opendkim.conf")
 
 
 class OpenDKIMCharm(ops.CharmBase):
@@ -31,6 +36,7 @@ class OpenDKIMCharm(ops.CharmBase):
         super().__init__(*args)
         self.framework.observe(self.on.install, self._install)
         self.framework.observe(self.on.upgrade_charm, self._install)
+        self.framework.observe(self.on.config_changed, self._reconcile)
         self.unit.open_port("tcp", OPENDKIM_MILTER_PORT)
 
     def _install(self, _: ops.EventBase) -> None:
@@ -38,6 +44,57 @@ class OpenDKIMCharm(ops.CharmBase):
         self.unit.status = ops.MaintenanceStatus("installing opendkim")
         apt.add_package(package_names=OPENDKIM_PACKAGE_NAME, update_cache=True)
         self.unit.status = ops.ActiveStatus()
+
+    def _reconcile(self, _: ops.EventBase) -> None:
+        """TODO."""
+        config = OpenDKIMConfig.from_charm(self)
+        # Jinja2 context
+        context = {str(config): str(config)}
+        env = Environment(
+            loader=FileSystemLoader("templates"),
+            autoescape=select_autoescape(),
+            keep_trailing_newline=True,
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+        template = env.get_template(str(OPENDKIM_CONFIG_TEMPLATE))
+        rendered = template.render(context)
+        render_file(OPENDKIM_CONFIG_PATH, rendered, 0o644)
+        # WHEN TO ?
+        # host.service_reload('opendkim')
+        # # Ensure service is running.
+        # host.service_start('opendkim')
+        self.unit.status = ops.ActiveStatus()
+
+
+# pylint: disable=too-few-public-methods,unused-argument
+class OpenDKIMConfig:
+    """TODO."""
+
+    @classmethod
+    def from_charm(cls, charm: OpenDKIMCharm) -> "OpenDKIMConfig":
+        """TODO.
+
+        Args:
+          charm: TODO
+
+        Return:
+          TODO.
+        """
+        return cls()
+
+
+def render_file(path: Path, content: str, mode: int) -> None:
+    """Write a content rendered from a template to a file.
+
+    Args:
+        path: Path object to the file.
+        content: the data to be written to the file.
+        mode: access permission mask applied to the
+            file using chmod (e.g. 0o640).
+    """
+    path.write_text(content, encoding="utf-8")
+    os.chmod(path, mode)
 
 
 if __name__ == "__main__":  # pragma: nocover
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,12 +3,16 @@
 
 """Unit tests."""
 
-from unittest.mock import MagicMock
+import os
+import stat
+import tempfile
+from pathlib import Path
+from unittest.mock import ANY, MagicMock
 
 import ops
 import ops.testing
 
-from charm import OpenDKIMCharm
+from charm import OpenDKIMCharm, render_file
 
 
 def test_install(monkeypatch):
@@ -30,3 +34,39 @@ def test_install(monkeypatch):
     assert len(out.opened_ports) == 1
     assert list(out.opened_ports)[0].port == 8892
     assert out.unit_status.name == ops.testing.ActiveStatus.name
+
+
+def test_basic_config(monkeypatch):
+    """
+    arrange: TODO.
+    act: TODO.
+    assert: TODO.
+    """
+    render_file_mock = MagicMock()
+    monkeypatch.setattr("charm.render_file", render_file_mock)
+
+    context = ops.testing.Context(
+        charm_type=OpenDKIMCharm,
+    )
+    base_state: dict[str, str] = {}
+    state = ops.testing.State(**base_state)
+    out = context.run(context.on.config_changed(), state)
+    assert out.unit_status.name == ops.testing.ActiveStatus.name
+    render_file_mock.assert_called_once_with(Path("/etc/opendkim.conf"), ANY, 0o644)
+    haproxy_conf_contents = render_file_mock.call_args_list[0].args[1]
+    assert haproxy_conf_contents == "sure it is not"
+
+
+def test_render_file():
+    """
+    arrange: TODO.
+    act: TODO.
+    assert: TODO.
+    """
+    with tempfile.TemporaryDirectory() as tmpdir:
+        content = "any text"
+        path = Path(tmpdir) / "onefile.txt"
+        render_file(path, content, 0o666)
+        st = os.stat(str(path))
+        assert oct(st.st_mode) == "0o100666"
+        assert path.read_text() == content
