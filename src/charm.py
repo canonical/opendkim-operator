afcb7f5881796daf9564d35993c7b6c33e54db11
Javier de la Puente <javier.delapuente@canonical.com>
2025-10-01 16:29:30 +0200

    wip first simple charm with unit tests no validation

--- src/charm.py
+++ src/charm.py
@@ -11,10 +11,14 @@ import logging
 import os
 import typing
 from pathlib import Path
+from typing import Optional, cast
 
 import ops
+import yaml
 from charms.operator_libs_linux.v0 import apt
+from charms.operator_libs_linux.v1 import systemd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pydantic import BaseModel, ValidationError, computed_field
 
 logger = logging.getLogger(__name__)
 
@@ -22,6 +26,22 @@ OPENDKIM_PACKAGE_NAME = "opendkim"
 OPENDKIM_MILTER_PORT = 8892
 OPENDKIM_CONFIG_TEMPLATE = Path("opendkim.conf.j2")
 OPENDKIM_CONFIG_PATH = Path("/etc/opendkim.conf")
+OPENDKIM_CONFIG_PATH = Path("/etc/opendkim.conf")
+OPENDKIM_KEYS_PATH = Path("/etc/dkimkeys")
+OPENDKIM_SIGNINGTABLE_PATH = OPENDKIM_KEYS_PATH / "signingtable"
+OPENDKIM_KEYTABLE_PATH = OPENDKIM_KEYS_PATH / "keytable"
+
+# https://datatracker.ietf.org/doc/html/rfc6376#section-5.4
+DEFAULT_SIGN_HEADERS = (
+    "From,Reply-To,Subject,Date,To,Cc"
+    ",Resent-From,Resent-Date,Resent-To,Resent-Cc"
+    ",In-Reply-To,References"
+    ",MIME-Version,Message-ID,Content-Type"
+)
+
+
+class InvalidCharmConfigError(Exception):
+    """Exception raised when the parsed charm config is invalid."""
 
 
 class OpenDKIMCharm(ops.CharmBase):
@@ -37,6 +57,7 @@ class OpenDKIMCharm(ops.CharmBase):
         self.framework.observe(self.on.install, self._install)
         self.framework.observe(self.on.upgrade_charm, self._install)
         self.framework.observe(self.on.config_changed, self._reconcile)
+        self.framework.observe(self.on.secret_changed, self._reconcile)
         self.unit.open_port("tcp", OPENDKIM_MILTER_PORT)
 
     def _install(self, _: ops.EventBase) -> None:
@@ -47,9 +68,16 @@ class OpenDKIMCharm(ops.CharmBase):
 
     def _reconcile(self, _: ops.EventBase) -> None:
         """TODO."""
-        config = OpenDKIMConfig.from_charm(self)
+        try:
+            config = OpenDKIMConfig.from_charm(self)
+        except InvalidCharmConfigError as exc:
+            logger.exception("Error validating the charm configuration.")
+            self.unit.status = ops.BlockedStatus(str(exc))
+            return
+
         # Jinja2 context
-        context = {str(config): str(config)}
+        context = config.model_dump()
+        logger.info("Javi context %s", context)
         env = Environment(
             loader=FileSystemLoader("templates"),
             autoescape=select_autoescape(),
@@ -57,19 +85,61 @@ class OpenDKIMCharm(ops.CharmBase):
             trim_blocks=True,
             lstrip_blocks=True,
         )
+
+        # JAVI validate here anything else?
+
+        # At this point, render all required files.
+        for keyname, keyvalue in config.private_keys.items():
+            keyfile = OPENDKIM_KEYS_PATH / f"{keyname}.private"
+            render_file(keyfile, keyvalue, 0o600)
+
+        signingtable = "\n".join(" ".join(row) for row in config.signingtable)
+        render_file(OPENDKIM_SIGNINGTABLE_PATH, signingtable, 0o644)
+
+        keytable = "\n".join(" ".join(row) for row in config.keytable)
+        render_file(OPENDKIM_KEYTABLE_PATH, keytable, 0o644)
+
         template = env.get_template(str(OPENDKIM_CONFIG_TEMPLATE))
         rendered = template.render(context)
         render_file(OPENDKIM_CONFIG_PATH, rendered, 0o644)
-        # WHEN TO ?
-        # host.service_reload('opendkim')
-        # # Ensure service is running.
-        # host.service_start('opendkim')
+        systemd.service_reload("opendkim")
         self.unit.status = ops.ActiveStatus()
 
 
 # pylint: disable=too-few-public-methods,unused-argument
-class OpenDKIMConfig:
-    """TODO."""
+class OpenDKIMConfig(BaseModel):
+    """TODO.
+
+    Attrs:
+        canonicalization: TODO
+        socket: TODO
+        signheaders: TODO
+        mode: TODO
+        internalhosts: TODO
+        signingtable: TODO
+        keytable: TODO
+        private_keys: TODO
+        signing_mode: TODO
+        signingtable_path: TODO
+        keytable_path: TODO
+    """
+
+    canonicalization: str = "relaxed/relaxed"
+    socket: str = f"inet:{OPENDKIM_MILTER_PORT}"
+    signheaders: str = DEFAULT_SIGN_HEADERS
+    mode: str = "sv"
+    internalhosts: str = "0.0.0.0/0"
+    signingtable: list[typing.Tuple[str, str]]
+    keytable: list[list[str]]
+    private_keys: dict[str, str]
+    signingtable_path: Path = OPENDKIM_SIGNINGTABLE_PATH
+    keytable_path: Path = OPENDKIM_KEYTABLE_PATH
+
+    @computed_field  # type: ignore[misc]
+    @property
+    def signing_mode(self) -> bool:
+        """TODO."""
+        return "s" in self.mode
 
     @classmethod
     def from_charm(cls, charm: OpenDKIMCharm) -> "OpenDKIMConfig":
@@ -78,10 +148,42 @@ class OpenDKIMConfig:
         Args:
           charm: TODO
 
+        Raises:
+          InvalidCharmConfigError: TODO
+
         Return:
           TODO.
         """
-        return cls()
+        signingtable_option = cast(Optional[str], charm.config.get("signingtable"))
+        if not signingtable_option:
+            raise InvalidCharmConfigError("empty signingtable configuration option")
+        try:
+            signingtable = yaml.safe_load(signingtable_option)
+        except yaml.YAMLError as exc:
+            raise InvalidCharmConfigError("Wrong signingtable format") from exc
+
+        keytable_option = cast(Optional[str], charm.config.get("keytable"))
+        if not keytable_option:
+            raise InvalidCharmConfigError("empty keytable configuration option")
+        try:
+            keytable = yaml.safe_load(keytable_option)
+        except yaml.YAMLError as exc:
+            raise InvalidCharmConfigError("Wrong keytable format") from exc
+
+        private_keys_secret_id = cast(Optional[str], charm.config.get("private-keys"))
+        if private_keys_secret_id is None:
+            raise InvalidCharmConfigError("empty private_keys configuration option")
+        private_keys_secret_id = private_keys_secret_id.replace("secret:", "")
+        secret = charm.model.get_secret(id=private_keys_secret_id)
+
+        # JAVI. Does this refresh has any implication in secret updating/rotation?
+        private_keys = typing.cast(dict[str, str], secret.get_content(refresh=True))
+        try:
+            return cls(signingtable=signingtable, keytable=keytable, private_keys=private_keys)
+        except ValidationError as exc:
+            logger.error(str(exc))
+            error_field_str = ",".join(f"{field}" for field in get_invalid_config_fields(exc))
+            raise InvalidCharmConfigError(f"Wrong config options: {error_field_str}") from exc
 
 
 def render_file(path: Path, content: str, mode: int) -> None:
@@ -95,6 +197,20 @@ def render_file(path: Path, content: str, mode: int) -> None:
     """
     path.write_text(content, encoding="utf-8")
     os.chmod(path, mode)
+
+
+def get_invalid_config_fields(exc: ValidationError) -> list[str]:
+    """Return a list on invalid config from pydantic validation error.
+
+    Args:
+        exc: The validation error exception.
+
+    Returns:
+        str: list of fields that failed validation.
+    """
+    logger.info(exc.errors())
+    error_fields = ["-".join([str(i) for i in error["loc"]]) for error in exc.errors()]
+    return error_fields
 
 
 if __name__ == "__main__":  # pragma: nocover
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -4,13 +4,15 @@
 """Unit tests."""
 
 import os
-import stat
 import tempfile
+import typing
 from pathlib import Path
-from unittest.mock import ANY, MagicMock
+from secrets import token_hex
+from unittest.mock import MagicMock
 
 import ops
 import ops.testing
+import pytest
 
 from charm import OpenDKIMCharm, render_file
 
@@ -36,25 +38,99 @@ def test_install(monkeypatch):
     assert out.unit_status.name == ops.testing.ActiveStatus.name
 
 
+@pytest.mark.parametrize(
+    "signingtable,keytable,private_keys,error_messages",
+    [
+        pytest.param(None, None, None, ["empty signingtable"], id="No config options"),
+        pytest.param("", "", {}, ["empty signingtable"], id="Empty config options"),
+        pytest.param(
+            "XX",
+            "XX",
+            {},
+            ["signingtable", "keytable"],
+            id="test3",
+        ),
+        pytest.param("*@", "", {}, ["format"], id="Wrong YAML"),
+    ],
+)
+def test_blocked_charm(signingtable, keytable, private_keys, error_messages):
+    """
+    arrange: TODO.
+    act: TODO.
+    assert: TODO.
+    """
+    context = ops.testing.Context(
+        charm_type=OpenDKIMCharm,
+    )
+
+    config: dict[str, str] = {}
+    secrets: ops.testing.Secret = {}
+    if signingtable is not None:
+        config["signingtable"] = signingtable
+    if keytable is not None:
+        config["keytable"] = keytable
+    if private_keys is not None:
+        secret_id = token_hex(20)[:20]
+        config["private-keys"] = f"secret:{secret_id}"
+        secrets = {ops.testing.Secret(id=secret_id, tracked_content=private_keys)}
+    base_state: dict[str, typing.Any] = {"config": config, "secrets": secrets}
+    state = ops.testing.State(**base_state)
+    out = context.run(context.on.config_changed(), state)
+    assert out.unit_status.name == ops.testing.BlockedStatus.name
+    for error_message in error_messages:
+        assert error_message in out.unit_status.message
+
+
 def test_basic_config(monkeypatch):
     """
     arrange: TODO.
     act: TODO.
     assert: TODO.
     """
+    systemd_reload_mock = MagicMock()
+    monkeypatch.setattr("charm.systemd.service_reload", systemd_reload_mock)
+
     render_file_mock = MagicMock()
     monkeypatch.setattr("charm.render_file", render_file_mock)
 
     context = ops.testing.Context(
         charm_type=OpenDKIMCharm,
     )
-    base_state: dict[str, str] = {}
+
+    keyname = "example.com-selector"
+    keytable = f"""
+[[selector._domainkey.example.com, example.com:selector:/etc/dkimkeys/{keyname}.private]]
+    """
+    signingtable = '[["*@example.com", "selector._domainkey.example.com"]]'
+    private_keys = {keyname: "PRIVATEKEY"}
+    secret_id = token_hex(20)[:20]
+    base_state: dict[str, typing.Any] = {
+        "config": {
+            "keytable": keytable,
+            "signingtable": signingtable,
+            "private-keys": f"secret:{secret_id}",
+        },
+        "secrets": {ops.testing.Secret(id=secret_id, tracked_content=private_keys)},
+    }
     state = ops.testing.State(**base_state)
     out = context.run(context.on.config_changed(), state)
+
     assert out.unit_status.name == ops.testing.ActiveStatus.name
-    render_file_mock.assert_called_once_with(Path("/etc/opendkim.conf"), ANY, 0o644)
-    haproxy_conf_contents = render_file_mock.call_args_list[0].args[1]
-    assert haproxy_conf_contents == "sure it is not"
+    systemd_reload_mock.assert_called_with("opendkim")
+    # There must be 4 calls to render_file.
+    assert render_file_mock.call_count == 4
+    expected_opendkim_conf = (Path(__file__).parent / "files/opendkim.conf").read_text()
+    render_file_mock.assert_any_call(Path(f"/etc/dkimkeys/{keyname}.private"), "PRIVATEKEY", 0o600)
+    render_file_mock.assert_any_call(
+        Path("/etc/dkimkeys/signingtable"), "*@example.com selector._domainkey.example.com", 0o644
+    )
+    render_file_mock.assert_any_call(
+        Path("/etc/dkimkeys/keytable"),
+        "selector._domainkey.example.com"
+        " example.com:selector:/etc/dkimkeys/example.com-selector.private",
+        0o644,
+    )
+    render_file_mock.assert_any_call(Path("/etc/opendkim.conf"), expected_opendkim_conf, 0o644)
 
 
 def test_render_file():
