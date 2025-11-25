4dfa7dfb67e5e4fb3bd6fe2e393c7014faf95254
Weii Wang <weii.wang@canonical.com>
2025-11-26 04:29:01 +0800

    Migrate to uv+ruff

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -8,7 +8,7 @@ import utils
 
 def test_logrotate_frequency():
     """Test log rotate frequency is updated."""
-    with open("tests/unit/files/logrotate_frequency", "r", encoding="utf-8") as f:
+    with open("tests/unit/files/logrotate_frequency", encoding="utf-8") as f:
         want = f.read()
     assert utils.update_logrotate_conf("tests/unit/files/logrotate", frequency="daily") == want
 
@@ -25,21 +25,21 @@ def test_logrotate_non_exists():
 
 def test_logrotate_retention():
     """Test update log rotate retention is correctly updated."""
-    with open("tests/unit/files/logrotate_retention", "r", encoding="utf-8") as f:
+    with open("tests/unit/files/logrotate_retention", encoding="utf-8") as f:
         want = f.read()
     assert utils.update_logrotate_conf("tests/unit/files/logrotate", retention=30) == want
 
 
 def test_logrotate_retention_no_dateext():
     """Test update log rotate retention without dateext."""
-    with open("tests/unit/files/logrotate_retention_no_dateext", "r", encoding="utf-8") as f:
+    with open("tests/unit/files/logrotate_retention_no_dateext", encoding="utf-8") as f:
         want = f.read()
     assert (
         utils.update_logrotate_conf("tests/unit/files/logrotate", retention=30, dateext=False)
         == want
     )
 
-    with open("tests/unit/files/logrotate_retention_no_dateext", "r", encoding="utf-8") as f:
+    with open("tests/unit/files/logrotate_retention_no_dateext", encoding="utf-8") as f:
         want = f.read()
     assert (
         utils.update_logrotate_conf(
