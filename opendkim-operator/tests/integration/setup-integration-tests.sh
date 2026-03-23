6686bde3e936cf42d169c8d6b6e0bdcfb3dc0c5c
Ali Ugur <ali.ugur@canonical.com>
2026-03-23 14:16:34 +0300

    chore: Use the locally built snap for tests

--- opendkim-operator/tests/integration/setup-integration-tests.sh
+++ opendkim-operator/tests/integration/setup-integration-tests.sh
@@ -4,13 +4,10 @@
 
 set -euxo pipefail
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install opendkim-tools
-
 # Build the opendkim snap from source for integration testing
 # dirname $0 is opendkim-operator/tests/integration; go up 3 levels to the repo root
 REPO_ROOT="$(cd "$(dirname "$0")/../../.." && pwd)"
 sudo snap install snapcraft --classic
 (cd "${REPO_ROOT}/opendkim-snap" && snapcraft pack)
-
+sudo snapcraft install "${REPO_ROOT}/opendkim-snap/opendkim_*.snap" --dangerous
 sudo docker run --rm -d -p 1080:1080 -p 25:1025 sj26/mailcatcher
